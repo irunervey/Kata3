af7fec136cad2543043e45d1b10956682d6e92a4
irune <irune@MSI>
2019-01-20 14:10:54 +0000

    Kata3 version 2

--- src/kata3/HistogramDisplay.java
+++ src/kata3/HistogramDisplay.java
@@ -9,9 +9,10 @@ import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
 
 public class HistogramDisplay extends ApplicationFrame{
-
-    public HistogramDisplay() {
+    private final Histogram<String> histogram;
+    public HistogramDisplay(Histogram<String> histogram) {
         super("HISTOGRAMA");
+        this.histogram=histogram;
         setContentPane(createPanel());
         pack();
     }
@@ -34,10 +35,9 @@ public class HistogramDisplay extends ApplicationFrame{
     
     private DefaultCategoryDataset createDataset(){
         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
-        dataSet.addValue(5,"","gmail.com");
-        dataSet.addValue(10,"","ulpgc.es");
-        dataSet.addValue(7,"","ull.es");
-        dataSet.addValue(2,"","hotmail.com");
+        for (String key : histogram.keySet()) {
+            dataSet.addValue(histogram.get(key), "", key);
+        }
         return dataSet;
     }
     
